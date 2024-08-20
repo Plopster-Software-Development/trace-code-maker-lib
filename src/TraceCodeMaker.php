c10042e9dee9100b7ae9a54c91ac27814803a2f1
Nicodav28 <esteveznicolas0@gmail.com>
2024-08-20 15:51:45 -0500

    ✨ (Readme.MD): Crear archivo Readme.MD para la biblioteca TraceCodeMaker 📝 (composer.json): Actualizar descripción de la biblioteca TraceCodeMaker El archivo Readme.MD se creó para proporcionar una descripción detallada de la biblioteca TraceCodeMaker, incluyendo requisitos, instrucciones de instalación, uso, contribuciones y licencia. La descripción en el archivo composer.json se actualizó para reflejar con precisión la funcionalidad y el propósito de la biblioteca TraceCodeMaker en Laravel.
    ✨ (TraceCodeMaker.php): Agregar método fetchOrCreateTraceCode para obtener o crear un código de traza. Añadir métodos auxiliares findExistingTraceCode, generateTraceCode, saveTraceCode, castToInt, createSuccessResponse y createErrorResponse para mejorar la funcionalidad y modularidad del código.
    Estos cambios se realizan para mejorar la legibilidad, mantenibilidad y reutilización del código. El nuevo método fetchOrCreateTraceCode simplifica la lógica al buscar un código de traza existente o crear uno nuevo según los parámetros proporcionados. Los métodos auxiliares ayudan a dividir la funcionalidad en partes más pequeñas y específicas, lo que facilita la comprensión y el mantenimiento del código.

--- src/TraceCodeMaker.php
+++ src/TraceCodeMaker.php
@@ -9,25 +9,84 @@ use Illuminate\Support\Str;
 
 class TraceCodeMaker
 {
-    public function generateTraceCode(string $service, string|int $httpCode, string $methodName, string $className, ?string $description = null): array
+
+    /**
+     * Fetches an existing trace code based on the provided parameters or creates a new one if it doesn't exist.
+     *
+     * @param  string      $service      The name of the service generating the trace code.
+     * @param  string|int  $httpCode     The HTTP status code associated with the trace code.
+     * @param  string      $methodName   The name of the method where the trace code is being generated.
+     * @param  string      $className    The name of the class where the trace code is being generated.
+     * @param  string|null $description  An optional description for the trace code.
+     * @return array                     An array containing the trace code or an error message.
+     */
+    public function fetchOrCreateTraceCode(string $service, string|int $httpCode, string $methodName, string $className, ?string $description = null): array
+    {
+        $existingTraceCode = $this->findExistingTraceCode($service, $httpCode, $methodName, $className);
+
+        if ($existingTraceCode) {
+            return $this->createSuccessResponse($existingTraceCode->trace_code);
+        }
+
+        $traceCode = $this->generateTraceCode($service, $httpCode, $methodName, $className);
+
+        return $this->saveTraceCode($service, $httpCode, $methodName, $className, $traceCode, $description);
+    }
+
+    /**
+     * Searches for an existing trace code in the database using the provided parameters.
+     *
+     * @param  string      $service    The name of the service generating the trace code.
+     * @param  string|int  $httpCode   The HTTP status code associated with the trace code.
+     * @param  string      $methodName The name of the method where the trace code is being generated.
+     * @param  string      $className  The name of the class where the trace code is being generated.
+     * @return object|null             The existing trace code object if found, otherwise null.
+     */
+    private function findExistingTraceCode(string $service, string|int $httpCode, string $methodName, string $className): ?object
+    {
+        return DB::table('trace_codes')
+            ->where('service', $service)
+            ->where('http_code', $httpCode)
+            ->where('method', $methodName)
+            ->where('class', $className)
+            ->first();
+    }
+
+    /**
+     * Generates a new trace code based on the provided parameters.
+     *
+     * @param  string      $service    The name of the service generating the trace code.
+     * @param  string|int  $httpCode   The HTTP status code associated with the trace code.
+     * @param  string      $methodName The name of the method where the trace code is being generated.
+     * @param  string      $className  The name of the class where the trace code is being generated.
+     * @return string                  The generated trace code.
+     */
+    private function generateTraceCode(string $service, string|int $httpCode, string $methodName, string $className): string
     {
         $serviceCode = strtoupper(substr($service, 0, 3));
         $methodHash = substr(md5($className . '.' . $methodName), 0, 5);
         $uniqueId = substr(time(), -3);
 
-        $traceCode = "{$serviceCode}-{$httpCode}-{$methodHash}-{$uniqueId}";
-
-        return $this->saveTraceCode($service, $httpCode, $methodName, $className, $traceCode, $description);
+        return "{$serviceCode}-{$httpCode}-{$methodHash}-{$uniqueId}";
     }
 
-    private function saveTraceCode(string $service, string|int $httpCode, string $methodName, string $className, string $traceCode, ?string $description = null)
+    /**
+     * Saves the generated trace code in the database and returns the status of the operation.
+     *
+     * @param  string      $service      The name of the service generating the trace code.
+     * @param  string|int  $httpCode     The HTTP status code associated with the trace code.
+     * @param  string      $methodName   The name of the method where the trace code is being generated.
+     * @param  string      $className    The name of the class where the trace code is being generated.
+     * @param  string      $traceCode    The trace code to be saved.
+     * @param  string|null $description  An optional description for the trace code.
+     * @return array                     An array containing the trace code or an error message.
+     */
+    private function saveTraceCode(string $service, string|int $httpCode, string $methodName, string $className, string $traceCode, ?string $description = null): array
     {
         try {
-            if (!$description) {
-                $description = Response::$statusTexts[$this->castToInt($httpCode)];
-            }
+            $description = $description ?? Response::$statusTexts[$this->castToInt($httpCode)];
 
-            DB::table('trace_codes')->insert([
+            $inserted = DB::table('trace_codes')->insert([
                 'id'          => Str::uuid()->toString(),
                 'trace_code'  => $traceCode,
                 'service'     => $service,
@@ -38,21 +97,53 @@ class TraceCodeMaker
                 'timestamp'   => Carbon::now()
             ]);
 
-            return [
-                "error"     => false,
-                "traceCode" => $traceCode
-            ];
+            if (!$inserted) {
+                throw new \Exception("Trace code could not be saved, please try again.");
+            }
+
+            return $this->createSuccessResponse($traceCode);
+
         } catch (\Throwable $th) {
-            return [
-                "error"   => true,
-                "message" => $th->getMessage()
-            ];
+            return $this->createErrorResponse($th->getMessage());
         }
     }
-
-    private function castToInt(string|int $param)
+    /**
+     * Casts a string or integer parameter to an integer.
+     *
+     * @param  string|int  $param  The parameter to be casted.
+     * @return int                 The casted integer value.
+     */
+    private function castToInt(string|int $param): int
     {
         return is_string($param) ? (int) $param : $param;
+    }
+
+    /**
+     * Creates a success response array containing the trace code.
+     *
+     * @param  string  $traceCode  The trace code to be included in the response.
+     * @return array               An array indicating success and containing the trace code.
+     */
+    private function createSuccessResponse(string $traceCode): array
+    {
+        return [
+            "error"     => false,
+            "traceCode" => $traceCode
+        ];
+    }
+
+    /**
+     * Creates an error response array containing the error message.
+     *
+     * @param  string  $message  The error message to be included in the response.
+     * @return array             An array indicating an error and containing the error message.
+     */
+    private function createErrorResponse(string $message): array
+    {
+        return [
+            "error"   => true,
+            "message" => $message
+        ];
     }
 
 }
